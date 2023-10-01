4d094d752b01646f7fe234efac78f04f08fe906b
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2023-10-01 18:12:18 +0300

    Add files via upload

---
+++ Java_Files/Individualka_nasledovanie/PassengerCar.java
@@ -0,0 +1,22 @@
+public class PassengerCar extends Avto {
+    private int seatingCapacity;
+
+    public PassengerCar(String brand, int power, double engineVolume, int seatingCapacity) {
+        super(brand, power, engineVolume);
+        this.seatingCapacity = seatingCapacity;
+    }
+
+    public int getSeatingCapacity() {
+        return seatingCapacity;
+    }
+    // Функция переназначения количества мест
+
+    public void setSeatingCapacity(int seatingCapacity) {
+        this.seatingCapacity = seatingCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"\nКоличество сидячих мест: "+seatingCapacity;
+    }
+}
