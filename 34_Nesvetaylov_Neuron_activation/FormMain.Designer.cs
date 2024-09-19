64167b5a33545017b7453833a6864353d267e545
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-09-19 15:30:42 +0300

    Add files via upload

---
+++ 34_Nesvetaylov_Neuron_activation/FormMain.Designer.cs
@@ -0,0 +1,356 @@
+﻿
+namespace _34_Nesvetaylov_Neuron_activation
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.numericUpDownTrue = new System.Windows.Forms.NumericUpDown();
+            this.buttonSaveTrainSample = new System.Windows.Forms.Button();
+            this.buttonSaveTestSample = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTrue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button2.Location = new System.Drawing.Point(482, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(376, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button3.Location = new System.Drawing.Point(588, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button4.Location = new System.Drawing.Point(376, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button5.Location = new System.Drawing.Point(482, 118);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            this.button6.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button6.Location = new System.Drawing.Point(588, 118);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Black;
+            this.button7.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button7.Location = new System.Drawing.Point(376, 224);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 10;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Black;
+            this.button8.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button8.Location = new System.Drawing.Point(482, 224);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 9;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Black;
+            this.button9.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button9.Location = new System.Drawing.Point(588, 224);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Black;
+            this.button10.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button10.Location = new System.Drawing.Point(376, 330);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 100);
+            this.button10.TabIndex = 13;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.White;
+            this.button11.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button11.Location = new System.Drawing.Point(482, 330);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 100);
+            this.button11.TabIndex = 12;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Black;
+            this.button12.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button12.Location = new System.Drawing.Point(588, 330);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(100, 100);
+            this.button12.TabIndex = 11;
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Black;
+            this.button13.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button13.Location = new System.Drawing.Point(376, 436);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(100, 100);
+            this.button13.TabIndex = 16;
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.White;
+            this.button14.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button14.Location = new System.Drawing.Point(482, 436);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(100, 100);
+            this.button14.TabIndex = 15;
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.Black;
+            this.button15.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button15.Location = new System.Drawing.Point(588, 436);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(100, 100);
+            this.button15.TabIndex = 14;
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button16.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button16.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold);
+            this.button16.ForeColor = System.Drawing.Color.Black;
+            this.button16.Location = new System.Drawing.Point(940, 12);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(183, 100);
+            this.button16.TabIndex = 17;
+            this.button16.Text = "РАСПОЗНАТЬ";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button17.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button17.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold);
+            this.button17.ForeColor = System.Drawing.Color.Black;
+            this.button17.Location = new System.Drawing.Point(940, 118);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(183, 100);
+            this.button17.TabIndex = 18;
+            this.button17.Text = "ОБУЧИТЬ";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button18.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button18.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold);
+            this.button18.ForeColor = System.Drawing.Color.Black;
+            this.button18.Location = new System.Drawing.Point(940, 224);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(183, 100);
+            this.button18.TabIndex = 19;
+            this.button18.Text = "ТЕСТ";
+            this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // numericUpDownTrue
+            // 
+            this.numericUpDownTrue.Location = new System.Drawing.Point(734, 141);
+            this.numericUpDownTrue.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDownTrue.Name = "numericUpDownTrue";
+            this.numericUpDownTrue.Size = new System.Drawing.Size(93, 20);
+            this.numericUpDownTrue.TabIndex = 20;
+            // 
+            // buttonSaveTrainSample
+            // 
+            this.buttonSaveTrainSample.AutoSize = true;
+            this.buttonSaveTrainSample.Location = new System.Drawing.Point(752, 260);
+            this.buttonSaveTrainSample.Name = "buttonSaveTrainSample";
+            this.buttonSaveTrainSample.Size = new System.Drawing.Size(171, 64);
+            this.buttonSaveTrainSample.TabIndex = 21;
+            this.buttonSaveTrainSample.Text = "Сохранить обучающий пример";
+            this.buttonSaveTrainSample.UseVisualStyleBackColor = true;
+            this.buttonSaveTrainSample.Click += new System.EventHandler(this.buttonSaveTrainSample_Click);
+            // 
+            // buttonSaveTestSample
+            // 
+            this.buttonSaveTestSample.AutoSize = true;
+            this.buttonSaveTestSample.Location = new System.Drawing.Point(752, 346);
+            this.buttonSaveTestSample.Name = "buttonSaveTestSample";
+            this.buttonSaveTestSample.Size = new System.Drawing.Size(162, 65);
+            this.buttonSaveTestSample.TabIndex = 22;
+            this.buttonSaveTestSample.Text = "Сохранить тестовый пример";
+            this.buttonSaveTestSample.UseVisualStyleBackColor = true;
+            this.buttonSaveTestSample.Click += new System.EventHandler(this.buttonSaveTestSample_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1177, 647);
+            this.Controls.Add(this.buttonSaveTestSample);
+            this.Controls.Add(this.buttonSaveTrainSample);
+            this.Controls.Add(this.numericUpDownTrue);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "FormMain";
+            this.Text = "Neurons";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTrue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.NumericUpDown numericUpDownTrue;
+        private System.Windows.Forms.Button buttonSaveTrainSample;
+        private System.Windows.Forms.Button buttonSaveTestSample;
+    }
+}
+
---
+++ 34_Nesvetaylov_Neuron_activation/FormMain.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;//используем классы чтобы работать с файлами
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _34_Nesvetaylov_Neuron_activation
+{
+    public partial class FormMain : Form
+    {
+        private double[] _pixels = new double[15] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private void ChangeState(Button b, int index)
+        {
+            if (b.BackColor == Color.Black)
+            {
+                b.BackColor = Color.White;
+                _pixels[index]= 1d;
+            }
+            else
+                if (b.BackColor == Color.White)
+            {
+                b.BackColor = Color.Black;
+                _pixels[index] = 0d;
+            }
+        }
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeState(button1, 0);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChangeState(button2, 0);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChangeState(button3, 0);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ChangeState(button4, 0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ChangeState(button5, 0);
+        }
+
+
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ChangeState(button6, 0);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ChangeState(button7, 0);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ChangeState(button8, 0);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ChangeState(button9, 0);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ChangeState(button10, 0);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ChangeState(button11, 0);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            ChangeState(button12, 0);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            ChangeState(button13, 0);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            ChangeState(button14, 0);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            ChangeState(button15, 0);
+        }
+
+
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void SaveTrain(decimal vale, double[] input)
+        {
+            string PathDir; // Каталог с файлом обучающей выборки
+            string NameFileTrain; // Имя файла обучающей выборки
+            PathDir = AppDomain.CurrentDomain.BaseDirectory;//получаем папку с exe
+            NameFileTrain = PathDir + "Train.txt";//Указали название файла
+            string[] tmpstr = new string[1];//Временная строка
+            tmpstr[0] = vale.ToString();
+            for(int i=0;i<15;i++)
+            {
+                tmpstr[0] += input[i].ToString();
+            }
+            File.AppendAllLines(NameFileTrain, tmpstr);
+        }
+
+        private void SaveTestSample(decimal vale, double[] input)
+        {
+            string PathDir; // Каталог с файлом обучающей выборки
+            string NameFileTestSample; // Имя файла обучающей выборки
+            PathDir = AppDomain.CurrentDomain.BaseDirectory;//получаем папку с exe
+            NameFileTestSample = PathDir + "TestSample.txt";//Указали название файла
+            string[] tmpstr_2 = new string[1];//Временная строка
+            tmpstr_2[0] = vale.ToString();
+            for (int i = 0; i < 15; i++)
+            {
+                tmpstr_2[0] += input[i].ToString();
+            }
+            File.AppendAllLines(NameFileTestSample, tmpstr_2);
+        }
+
+        private void buttonSaveTrainSample_Click(object sender, EventArgs e)
+        {
+            SaveTrain(numericUpDownTrue.Value, _pixels);
+        }
+
+        private void buttonSaveTestSample_Click(object sender, EventArgs e)
+        {
+            SaveTestSample(numericUpDownTrue.Value, _pixels);
+        }
+    }
+}
